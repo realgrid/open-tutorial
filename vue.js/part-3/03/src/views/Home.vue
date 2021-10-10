f92a48129b2b675197bec59b0c4473017cedb693
Ryu <ryujt658@gmail.com>
2021-10-10 20:35:32 +0900

    Vue.js 목록을 테이블 형태로 표시하기 예제 추가

---
+++ vue.js/part-3/03/src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <el-row>
+    <el-col :span="2"> </el-col>
+    <el-col :span="20">
+      <el-table :data="articles" style="width: 100%">
+          <el-table-column prop="id" label="id" width="80"> </el-table-column>
+          <el-table-column prop="userId" label="userId" width="120"> </el-table-column>
+          <el-table-column prop="title" label="title"> </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+import apiBoard from '@/api/board'
+
+export default {
+  data() {
+    return {
+      articles: null,
+    }
+  },
+  mounted() {
+    apiBoard.getArticles(0)
+      .then((response) => {
+        console.log("getArticles", response);
+        this.articles = response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+}
+</script>
