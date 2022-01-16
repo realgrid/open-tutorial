a5be0e7d9244eb60f184ae74b287683b005e018f
Ryu <ryujt658@gmail.com>
2022-01-16 22:33:48 +0900

    Revert "=> 함수로 일부 변경"
    This reverts commit ebdcddf905252be7b3ae434bd8bebe2ed9e53424.

--- vue.js/part-4/04/src/views/manager/List.vue
+++ vue.js/part-4/04/src/views/manager/List.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="main-body">
         <div class="toolbar">
-            <el-button @click="showFormView" type="primary">수정</el-button>
             <el-button @click="deleteRow" type="warning">삭제</el-button>
-
-            <el-button v-if="!filtering" @click="setFltering(true)" type="Default">필터 해제됨 </el-button>
-            <el-button v-if="filtering" @click="setFltering(false)" type="danger">필터 적용중</el-button>
         </div>
 
         <div id="realgrid" style="width: 100%; height: 90vh"></div>
@@ -19,11 +15,6 @@ import apiManagers from "@/api/manager";
 import md5 from "md5";
 
 export default {
-    data() {
-        return {
-            filtering: false,
-        };
-    },
     mounted() {
         this.provider = new RealGrid.LocalDataProvider();
         this.gridView = new RealGrid.GridView("realgrid");
@@ -42,65 +33,12 @@ export default {
         ]);
 
         this.gridView.setColumns([
-            { name: "email", fieldName: "email", width: 120 },
-            { name: "pw", fieldName: "pw" },
-            { name: "name", fieldName: "name" },
-
-            {
-                name: "rule",
-                fieldName: "rule",
-                editor: {
-                    type: "dropdown",
-                    domainOnly: true,
-                    values: ["기본관리자", "관리자1", "관리자2", "관리자3"],
-                    labels: ["기본관리자", "관리자1", "관리자2", "관리자3"],
-                },
-            },
-
-            {
-                name: "phoneNumber",
-                fieldName: "phoneNumber",
-                width: 120,
-                textFormat: "([0-9]{3})([0-9]{4})([0-9]{4}); $1-$2-$3",
-                editor: {
-                    type:"text",
-                    mask: "000-0000-0000",
-                },
-            },
+            { name: "email",       fieldName: "email",       width: 120 },
+            { name: "pw",          fieldName: "pw" },
+            { name: "name",        fieldName: "name"},
+            { name: "rule",        fieldName: "rule"},
+            { name: "phoneNumber", fieldName: "phoneNumber", width: 120 },
         ]);
-
-        this.formView = this.gridView._view.container.formView;
-        this.formView.visible = false;
-        this.formView.options.modal = true;
-        this.formView.options.saveLabel = "저장";
-        this.formView.options.cancelLabel = "취소";
-        this.formView.model.header.height = 40;
-        this.formView.options.autoClose = true;
-
-        this.formView.model.load({
-            items: [
-                {
-                    header: "email",
-                    column: "email",
-                },
-                {
-                    header: "비번",
-                    column: "pw",
-                },
-                {
-                    header: "이름",
-                    column: "name",
-                },
-                {
-                    header: "권한",
-                    column: "rule",
-                },
-                {
-                    header: "전화번호",
-                    column: "phoneNumber",
-                },
-            ],
-        });
 
         this.gridView.onCellEdited = function (gridView, itemIndex, row, field) {
             if (field === 1) {
@@ -146,13 +84,6 @@ export default {
     },
 
     methods: {
-        showFormView: function () {
-            if (this.gridView.getCurrent().itemIndex === -1) {
-                this.$message.error("편집할 데이터를 선택해주세요.");
-                return;
-            }
-            this.formView.visible = true;
-        },
         deleteRow: async function () {
             let itemIndex = this.gridView.getCurrent().itemIndex;
             if (itemIndex === -1) {
@@ -175,14 +106,6 @@ export default {
                 .catch((e) => {
                     console.log(e);
                 });
-        },
-        setFltering: function (value) {
-            this.filtering = value;
-            if (value) {
-                this.gridView.setColumnProperty("rule", "autoFilter", true);
-            } else {
-                this.gridView.setColumnProperty("rule", "autoFilter", false);
-            }
         },
     },
 };
