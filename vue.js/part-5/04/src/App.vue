7b299b8f54ff3690b578d06b7a3afd8a68f16aa5
Ryu <ryujt658@gmail.com>
2022-06-04 23:24:28 +0900

    Part-5 04 예제 추가

---
+++ vue.js/part-5/04/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+    <div>
+        <div style="height: 32px; position: fixed">
+            <button @click="exportPdf()">pdf</button>
+            &nbsp;&nbsp;
+
+            <button @click="exportImage('png')">png</button>
+            <button @click="exportImage('jpeg')">jpeg</button>
+            <button @click="exportImage('gif')">gif</button>
+            <button @click="exportImage('tif')">tif</button>
+            <button @click="exportImage('tiff')">tiff</button>
+            &nbsp;&nbsp;
+
+            <button @click="exportDocument('hwp')">hwp</button>
+            <button @click="exportDocument('docx')">docx</button>
+            &nbsp;&nbsp;
+
+            <button @click="print()">print</button>
+        </div>
+
+        <div style="height: 32px"></div>
+
+        <div id="realreport" class="scroller"></div>
+
+        <iframe id="printPreview" src="/print.html" style="display: none"></iframe>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+import reportForm from "./data/reportForm";
+import reportData from "./data/reportData";
+
+export default {
+    mounted() {
+        this.viewer = new RealReport.ReportViewer("realreport", reportForm);
+        this.viewer.dataSet = reportData;
+        this.viewer.preview();
+    },
+
+    methods: {
+        exportPdf() {
+            const pdfFonts = [
+                {
+                    name: "regular",
+                    content: malgun,
+                    style: "normal",
+                    weight: "normal",
+                },
+                {
+                    name: "bold",
+                    content: malgunBold,
+                    style: "normal",
+                    weight: "bold",
+                },
+            ];
+            this.viewer.exportPdf(pdfFonts);
+        },
+
+        exportImage(imageType) {
+            this.viewer.exportImage({ type: imageType });
+        },
+
+        exportDocument(docType) {
+            this.viewer.exportDocument({ type: docType });
+        },
+
+        print() {
+            const container = printPreview.contentWindow.document.getElementById("realreport");
+            container.innerHTML = this.viewer.getHtml();
+            printPreview.contentWindow.print();
+        },
+    },
+};
+</script>
+
+<style scoped>
+.scroller {
+    flex: 1;
+    direction: ltr;
+    height: 90vh;
+    width: 100%;
+    overflow: auto;
+    position: relative;
+}
+</style>
