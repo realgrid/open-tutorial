a35a5df97d475ca82241739f543bd3913d36fd78
Ryu <ryujt658@gmail.com>
2022-06-18 15:36:19 +0900

    Part-4 10 예제 완성

--- vue.js/part-4/10/src/components/RealReport.vue
+++ vue.js/part-4/10/src/components/RealReport.vue
@@ -1,15 +1,50 @@
 <template>
     <div>
-
+        <el-row style="padding: 4px;">
+            <el-button @click="print"><i class="fas fa-print" /></el-button>
+            <el-button @click="exportPdf"><i class="fas fa-file-pdf" /></el-button>
+        </el-row>
+        <el-row>
+            <div id="realreport"></div>
+        </el-row>
+        <iframe id="printPreview" src="/print.html" style="display: none"></iframe>
     </div>
 </template>
 
 <script>
+/* eslint-disable */
+
 export default {
+    methods: {
+        preview(form, data) {
+            this.viewer = new RealReport.ReportViewer("realreport", form);
+            this.viewer.dataSet = data;
+            this.viewer.preview();
+        },
 
-}
+        print() {
+            const container = printPreview.contentWindow.document.getElementById("realreport");
+            container.innerHTML = this.viewer.getHtml();
+            printPreview.contentWindow.print();
+        },
+
+        exportPdf() {
+            const pdfFonts = [
+                {
+                    name: "regular",
+                    content: malgun,
+                    style: "normal",
+                    weight: "normal",
+                },
+                {
+                    name: "bold",
+                    content: malgunBold,
+                    style: "normal",
+                    weight: "bold",
+                },
+            ];
+            this.viewer.exportPdf(pdfFonts);
+        },
+    }
+};
 </script>
-
-<style scoped>
-
-</style>
--- vue.js/part-4/10/src/views/report/member/SalaryMonthly.vue
+++ vue.js/part-4/10/src/views/report/member/SalaryMonthly.vue
@@ -1,72 +1,27 @@
 <template>
-    <div id="realreport" class="scroller"></div>
+    <RealReport ref="realReport" />
 </template>
 
 <script>
 /* eslint-disable */
 
-const reportForm = {
-    page: {
-        pageHeader: {
-            items: [
-                {
-                    type: "text",
-                    text: "리포트 제목",
-                    height: "100px",
-                    width: "100%",
-                    styles: {
-                        fontSize: "25px",
-                        fontWeight: "bold",
-                        backgroundColor: "#ccc",
-                    },
-                },
-            ],
-        },
-        body: {
-            items: [
-                {
-                    type: "text",
-                    text: "RealReport는 Javascript를 이용해 간단하게 리포트를 출력 할 수 있습니다.",
-                    height: "100px",
-                    styles: {
-                        fontSize: "13px",
-                    },
-                },
-                {
-                    type: "text",
-                    data: "myData",
-                    value: "company",
-                    styles: {
-                        color: "blue",
-                    },
-                },
-                {
-                    type: "text",
-                    data: "myData",
-                    value: "age",
-                    styles: {
-                        fontWeight: "bold",
-                    },
-                },
-            ],
-        },
-    },
-};
-
-const reportData = {
-    myData: {
-        values: {
-            company: "우리테크",
-            age: 10,
-        },
-    },
-};
+import RealReport from '@/components/RealReport';
+import salary from '@/api/salary';
+import salaryMonthly from '@/report/salaryMonthly';
 
 export default {
+    components: {
+        RealReport
+    },
+
     mounted() {
-        this.viewer = new RealReport.ReportViewer("realreport", reportForm);
-        this.viewer.dataSet = reportData;
-        this.viewer.preview();
+        salary.monthly()
+            .then((response) => {
+                this.$refs.realReport.preview(salaryMonthly, response.data);
+            })
+            .catch((e) => {
+                console.log(e);
+            });
     },
 };
 </script>
